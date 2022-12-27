7af1612a8d58ded884b8c2351ae5dd481d63d679
Anand747 <anand@thelpinecode.com>
2022-12-27 20:45:19 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,7 @@
+Eslint-config
+
+Eslint configuration for desktop application
+
+The latest code in main branch
+
+The latest tag's data will be used in desktop app 
---
+++ index.js
@@ -0,0 +1,36 @@
+module.exports = {
+  extends: [
+    "airbnb",
+    "airbnb-typescript",
+    "airbnb/hooks",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:jest/recommended",
+    "plugin:react/recommended",
+    "plugin:import/recommended",
+    "plugin:jest/recommended",
+    "plugin:react-hooks/recommended",
+    "plugin:promise/recommended",
+    "plugin:compat/recommended",
+    "plugin:prettier/recommended",
+  ],
+  env: {
+    browser: true,
+    node: true,
+  },
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaVersion: 2020,
+    sourceType: "module",
+  },
+  rules: {
+    "@typescript-eslint/explicit-function-return-type": "off",
+    "@typescript-eslint/explicit-module-boundary-types": "off",
+    "@typescript-eslint/no-var-requires": "off",
+    "no-param-reassign": ["error", { props: false }],
+  },
+  settings: {
+    "import/parsers": {
+      "@typescript-eslint/parser": [".ts", ".tsx"],
+    },
+  },
+};
